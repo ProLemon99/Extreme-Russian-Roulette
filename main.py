055f2b9691c493f5493e242e855297185485cf2d
Levin Shao <levinshao628@gmail.com>
2025-05-08 15:09:45 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 import random
 
+print("Before you get started, just know what you're getting yourself into.")
+print("You must know that this script is extremely dangerous, as it will attempt to delete System32.")
+print("You have a chance to turn back now. Or you could gamble.")
+print("Note: you won't get anything in reward for being lucky either.")
+
 nice_number = random.randint(1, 6)
 
 while True:
